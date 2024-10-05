861ca40d26d1f918017712e1f6df36fcc2bd0030
Miheretab Mengesha <mengeshamiheretab36@gmail.com>
2024-10-05 09:27:36 +0300

    update2

--- js/script.js
+++ js/script.js
@@ -13,3 +13,17 @@ function userScroll() {
 }
 
 document.addEventListener('DOMContentLoaded', userScroll);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    var navCollapse = document.querySelector('.navbar-collapse');
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            if (navCollapse.classList.contains('show')) {
+                navCollapse.classList.remove('show');
+            }
+        });
+    });
+});
